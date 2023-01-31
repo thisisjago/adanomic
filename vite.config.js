ff5fa274a02b6123a20cf083f001f1cd99c1567b
Jose Germosen <josegermosen@protonmail.com>
2023-01-31 13:43:48 -0400

    added some packages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 // vite.config.js
+import generateSitemap from 'vite-ssg-sitemap'
+
 export default {
     base: '/',
     build: {
       target: 'esnext'
-    }
+    },
+    ssgOptions: {
+      onFinished() { generateSitemap() },
+    },
     // config options
   }
-  
-  
   
